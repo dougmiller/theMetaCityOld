7fb90f44cb480856fc5b9dc7bf4e785dd719d2ef
Douglas Miller <dougmiller@themetacity.com>
2012-08-27 22:23:53 +1000

    Changed the output ot be the ACTUAL linked file not one left over from testing.

--- utils/jscombiner.sh
+++ utils/jscombiner.sh
@@ -10,13 +10,13 @@ cat searcher.js > tmcscripts.js
 
 echo 'Removing unessary characters'
 
-sed -i 's/ *\/\/.*//g' screen.js      # Dont need comments (and they become greedy when everything is on a single line)
-sed -i ':a;N;$!ba;s/\n//g' screen.js  # New lines
-sed -i 's/\t//g' screen.js            # Tabs
-sed -i 's/ () /()/g' screen.js        # Spaces in 'function () {'
-sed -i 's/ \(=\+\) /\1/g' screen.js   # Spaces in 'x = y', 'x == y', 'x === y'
-sed -i 's/) {/){/g' screen.js         # End of parameter list and opening curly brace
-sed -i 's/if (/if(/g' screen.js       # End of if and opening round bracket
-sed -i 's/, \(.\)/,\1/g' screen.js    # Comma space anything
+sed -i 's/ *\/\/.*//g' tmcscripts.js      # Dont need comments (and they become greedy when everything is on a single line)
+sed -i ':a;N;$!ba;s/\n//g' tmcscripts.js  # New lines
+sed -i 's/\t//g' tmcscripts.js            # Tabs
+sed -i 's/ () /()/g' tmcscripts.js        # Spaces in 'function () {'
+sed -i 's/ \(=\+\) /\1/g' tmcscripts.js   # Spaces in 'x = y', 'x == y', 'x === y'
+sed -i 's/) {/){/g' tmcscripts.js         # End of parameter list and opening curly brace
+sed -i 's/if (/if(/g' tmcscripts.js       # End of if and opening round bracket
+sed -i 's/, \(.\)/,\1/g' tmcscripts.js    # Comma space anything
 
 echo 'Finished combining all JavaScript files'
