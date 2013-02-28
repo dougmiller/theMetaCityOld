29bd7658886e732f7a5e648465f93e353f8db450
Douglas Miller <dougmiller@themetacity.com>
2013-02-28 21:29:49 +1000

    Changed the mounter to be a bit more helpful when you get arguments wrong and a bit more flexible when adding in new mounts.

--- utils/encmount.sh
+++ utils/encmount.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
-if [ -z "$2" ]; then
+if [[ $# -ne 2 || ( $1 != "mount" && $1 != "unmount" ) ]]; then
   echo "Usage:"
-  echo "encmount.sh mount remotepoint     # mounts the remote file system"
-  echo "encmount.sh unmount remotepoint   # unmounts the remote file system"
-
+  echo "encmount.sh mount remotepoint       # mount the remote file system"
+  echo "encmount.sh unmount remotepoint   # unmount the remote file system"
+  exit -1
 fi
 
-commands=(mount unmount)
+declare -A mountInfo
 
-if [ "$1" == "${commands[0]}" ];  then      # Mounts the file system
-  if [ "$2" == "tmc" ];  then
-    sshfs doug@enc:/srv/http/tmc /media/tmc   # Port set in ssh config (not sshd config). Directory needs to exist and need rights on it.
+# Add mounts as needed
+case $2 in
+  "tmc")
+    mountInfo=( [name]="the MetaCity"
+                          [ref]="tmc"
+                          [mountCommand]="sshfs doug@enc:/srv/http/tmc /media/tmc"
+                          [unmountCommand]="sudo umount /media/tmc");;
+  *) 
+    echo "Unknown mount. Please provide a known mount."
+    exit 2;;
+esac
+
+# Proper command found, time to execute it
+echo "I am going to: "$1 ${mountInfo[name]}
+
+if [ $1 == "mount" ]; then
+  ${mountInfo[mountCommand]}
+  if [[ $? -ne 0 ]]; then
+    echo "Something went wrong with mounting. See errors above."
+  else
+    echo "Mounted successfully."
   fi
-
-elif [ "$1" == "${commands[1]}" ];  then    # Unmounts the file system
-  if [ "$2" == "tmc" ];  then
-    sudo umount /media/tmc 
+elif  [ $1 == "unmount" ]; then
+  ${mountInfo[unmountCommand]}
+  if [[ $? -ne 0 ]]; then
+    echo "Something went wrong with unmounting. See errors above."
+  else
+    echo "Unmounted successfully."
   fi
-
-else
-  echo "Don't know what that is."
 fi
