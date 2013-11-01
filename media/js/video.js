d53bc49350596502b5f726cf53ab4a3b2ae6ab51
Douglas Miller <dougmiller@themetacity.com>
2013-11-01 23:14:08 +1000

    Video controls are a stable-yet-not-too-functional state that I am happy with. Still more styles to get to beut I am quite hapy with it. Fade i nadn out work as intended with mouseenter and mouseleave rahter than mousein and mouseout plus clearQueue(). Also the controls no longer appear at the start and require the mouse over for the first time. In a future release a poster like system will be in effect which will hide that until clicked anyway.

--- media/js/video.js
+++ media/js/video.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     'use strict';
-    var videos = document.getElementsByTagName('video'), i, controlsTimeout;
+    var videos = $("video");
 
     function isVideoPlaying(video) {
         return (video.paused || video.ended || video.seeking || video.readyState < video.HAVE_FUTURE_DATA);
@@ -16,82 +16,74 @@ $(document).ready(function () {
         }
     }
 
-    for (i = 0; i < videos.length; i += 1) {
-        (function (vidIndex) {
-            var vid = videos[vidIndex], vidBox = vid.parentNode, controlsBox, vidOffset, playPauseButton, muteButton, progressBar, heightsTogether;
-            vid.controls = false;
+    $(videos).each(function (vidIndex) {
+        var vidBox = this.parentNode, controlsBox, vidOffset, playPauseButton, progressBar, heightsTogether;
+        this.controls = false;
 
-            vidOffset = $(vidBox).offset();
+        vidOffset = $(vidBox).offset();
 
-            controlsBox = document.createElement('div');
-            controlsBox.setAttribute("class", 'videoControls');
-            //controlsBox.style.left = controlsOffset.left + (parseInt(vidBox.offsetWidth/2, 10)) - $(controlsBox).width()/2 + "px";
-            //controlsBox.style.top = controlsOffset.top + vidBox.offsetHeight - 4 + "px";
+        controlsBox = document.createElement('div');
+        controlsBox.setAttribute("class", 'videoControls');
+        $(controlsBox).css({opacity: 0});   // Start invisible, better here than CSS
 
+        heightsTogether = Math.floor(vidOffset.top + $(vidBox).height() - 4 - $(controlsBox).height());
+        $(controlsBox).offset({top: heightsTogether});
+        $(controlsBox).width($(vidBox).width() - 2);
 
-            heightsTogether = Math.floor(vidOffset.top + $(vidBox).height() - 4 - $(controlsBox).height());
-            $(controlsBox).offset({top: heightsTogether});
-            $(controlsBox).width($(vidBox).width() - 2);
+        playPauseButton = document.createElement('img');
+        playPauseButton.src = "/media/site-images/smallplay.svg";
+        playPauseButton.setAttribute("class", 'playPauseButton');
 
-            playPauseButton = document.createElement('img');
-            playPauseButton.src = "/media/site-images/smallplay.svg";
-            playPauseButton.setAttribute("class", 'playPauseButton');
+        playPauseButton.addEventListener('click', (function (vid, playPauseButton) {
+            return function () {
+                playPause(vid, playPauseButton);
+            };
+        }(this, playPauseButton)));
 
-            playPauseButton.addEventListener('click', (function (vid, playPauseButton) {
-                return function () {
-                    playPause(vid, playPauseButton);
-                };
-            }(vid, playPauseButton)));
+        controlsBox.appendChild(playPauseButton);
 
-            controlsBox.appendChild(playPauseButton);
+        progressBar = document.createElement("progress");
+        progressBar.max = 1;  // Without these values the progress bar gets put into 'indeterminate mode
+        progressBar.value = 0;  //And that breaks things so we put them in even though they are redundant
 
-            progressBar = document.createElement("progress");
-            progressBar.max = 1;  // Without these values the progress bar gets put into 'indeterminate mode
-            progressBar.value = 0;  //And that breaks things so we put them in even though they are redundant
+        progressBar.addEventListener('click', function() {
+            this.currentTime = progressBar.value;
+        }, false);
 
-            progressBar.addEventListener('click', function() {
-                vid.currentTime = progressBar.value;
-            }, false);
+        controlsBox.appendChild(progressBar);
 
-            controlsBox.appendChild(progressBar);
+        // When the video is hovered over, show the controls
+        vidBox.addEventListener('mouseenter', (function (controlsBox) {
+            return function () {
+                $(controlsBox).fadeTo(400, 1);
+                $(controlsBox).clearQueue();
+            };
+        }(controlsBox)));
 
-            muteButton = document.createElement('img');
-            muteButton.src = "/media/site-images/mute.svg";
-            muteButton.setAttribute("class", 'muteButton');
+        // When the hover leaves, hide the controls
+        vidBox.addEventListener('mouseleave', (function (controlsBox) {
+            return function () {
+                $(controlsBox).fadeTo(400, 0);
+                $(controlsBox).clearQueue();
+            };
+        }(controlsBox)));
 
-            // When the video is hovered over, show the controls
-            vidBox.addEventListener('mouseenter', (function (controlsBox) {
-                return function () {
-                    $(controlsBox).fadeTo(400, 1);
-                    $(controlsBox).clearQueue();
-                };
-            }(controlsBox)));
+        // If the video itself is clicked
+        this.addEventListener('click', (function (vid, playPauseButton) {
+            return function () {
+                playPause(vid, playPauseButton);
+            };
+        }(this, playPauseButton)));
 
-            // When the hover leaves, hide the controls
-            vidBox.addEventListener('mouseleave', (function (controlsBox) {
-                return function () {
-                    $(controlsBox).fadeTo(400, 0);
-                    $(controlsBox).clearQueue();
-                };
-            }(controlsBox)));
+        this.addEventListener('timeupdate', function() {
+            progressBar.value = this.currentTime / this.duration;
+        }, false);
 
-            // If the video itself is clicked
-            vid.addEventListener('click', (function (vid, playPauseButton) {
-                return function () {
-                    playPause(vid, playPauseButton);
-                };
-            }(vid, playPauseButton)));
-
-            vid.addEventListener('timeupdate', function() {
-                progressBar.value = vid.currentTime / vid.duration;
-            }, false);
-
-            // Add whe whole lot onto the page
-            if (vid.nextSibling) {
-                vid.parentNode.insertBefore(controlsBox, vid.nextSibling);
-            } else {
-                vid.parentNode.appendChild(controlsBox);
-            }
-        }(i));
-    }
+        // Add whe whole lot onto the page
+        if (this.nextSibling) {
+            this.parentNode.insertBefore(controlsBox, this.nextSibling);
+        } else {
+            this.parentNode.appendChild(controlsBox);
+        }
+    });
 });
