8f508ba7a76c5e2626474b0526a34c4a15d41785
Douglas Miller <dougmiller@themetacity.com>
2016-04-02 14:24:08 +1000

    Moved to npm system for css

---
+++ media/postCSS.js
@@ -0,0 +1,35 @@
+console.log("CSS changed. Processing...");
+
+var postcss = require('postcss');
+var fs = require('fs');
+
+var postcss_media_variables = require('postcss-media-variables');
+var postcss_partial_import = require('postcss-partial-import');
+var postcss_css_variables = require('postcss-css-variables');
+var postcss_nested = require('postcss-nested');
+var postcss_discard_comments = require('postcss-discard-comments');
+var postcss_autoprefixer = require('autoprefixer');
+
+var options = {
+    from: 'css/style.css',
+    to: 'theMetaCityMedia/static/css/style.css',
+    map: { inline: true }
+};
+
+var css = fs.readFileSync("css/style.css", "utf8");
+
+postcss([
+        postcss_partial_import,
+        postcss_media_variables,
+        postcss_css_variables,
+        postcss_media_variables,
+        postcss_nested,
+        postcss_discard_comments,
+        postcss_autoprefixer
+    ])
+    .process(css, options)
+    .then(function (result) {
+        fs.writeFileSync('theMetaCityMedia/static/css/style.css', result.css);
+    });
+
+console.log("CSS processed");
